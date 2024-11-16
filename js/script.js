0a3da38095a42f7c0a786dd32adf3baaefe41845
kushk4856 <kushk4998@gmail.com>
2024-11-16 13:56:31 +0530

    sticky header featured added

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ const closeBtn = document.querySelector(".offcanvas-close-btn");
 const offCanvas = document.querySelector(".offcanvas__area");
 const bodyOverlay = document.querySelector(".body-overlay");
 const hasDropdown = document.querySelectorAll(".has-dropdown");
+const navbar = document.getElementById("header-sticky");
 
+/* 
+========================
+for Toggle mobile navbar 
+=============================
+ */
 closeMenuBtn.addEventListener("click", () => {
   offCanvas.classList.toggle("offcanvas-opened");
   bodyOverlay.classList.toggle("opened");
@@ -13,9 +19,26 @@ openMenuBtn.addEventListener("click", () => {
   offCanvas.classList.toggle("offcanvas-opened");
   bodyOverlay.classList.toggle("opened");
 });
-
+/* 
+========================
+for expanded dropdown 
+=============================
+ */
 hasDropdown.forEach((el) => {
   el.addEventListener("click", () => {
     el.classList.toggle("expanded");
   });
+});
+
+/* 
+========================
+for Sticky header
+=============================
+ */
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 200) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 });
