a6a5e8bbc3a07c4a43506b39376d4ced95239de8
kushk4856 <kushk4998@gmail.com>
2024-11-16 13:45:07 +0530

    some icons added and mobile nav dropdown feature added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const openMenuBtn = document.querySelector(".open_menu_btn");
 const closeBtn = document.querySelector(".offcanvas-close-btn");
 const offCanvas = document.querySelector(".offcanvas__area");
 const bodyOverlay = document.querySelector(".body-overlay");
+const hasDropdown = document.querySelectorAll(".has-dropdown");
 
 closeMenuBtn.addEventListener("click", () => {
   offCanvas.classList.toggle("offcanvas-opened");
@@ -11,4 +12,10 @@ closeMenuBtn.addEventListener("click", () => {
 openMenuBtn.addEventListener("click", () => {
   offCanvas.classList.toggle("offcanvas-opened");
   bodyOverlay.classList.toggle("opened");
+});
+
+hasDropdown.forEach((el) => {
+  el.addEventListener("click", () => {
+    el.classList.toggle("expanded");
+  });
 });
